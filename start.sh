44ecd7034ce13bc5cf0b7ef825b65292bb56cded
M4T01 <128631144+drmatoi@users.noreply.github.com>
2023-06-02 20:43:59 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,28 @@
+ #!/bin/bash
+
+# colour 
+Black="\033[1;30m"       # Black
+Red="\033[1;31m"         # Red
+Green="\033[1;32m"       # Green
+Yellow="\033[1;33m"      # Yellow
+Blue="\033[1;34m"        # Blue
+Purple="\033[1;35m"      # Purple
+Cyan="\033[1;36m"        # Cyan
+White="\033[1;37m"       # White
+clear 
+echo -e "$Red Welcome to Dr.Matoi´s Collection. \033[1;31m"
+sleep 5.0
+clear
+echo -e "$Cyan 
+╔══════════════════════════════════════════════════════════╦════════════════════╗
+║     Visti drmatoi.github.io  - t.me/moddingtool          ║   By Mr.Matoi      ║
+║══════════════════════════════════════════════════════════╬════════════════════╣
+║     Acces to a big collection of Bash, Shell and         ║     ▬▬ι═══════ﺤ    ║
+║                   phyton scripts!                        ║                    ║
+║         Please report not working links in "Issues"      ╠════════════════════╣
+║                  github.com/drmatoi/matoi                ║     Version 1.0    ║
+╚══════════════════════════════════════════════════════════╩════════════════════╝
+
+
+
+\033[1;36m"
